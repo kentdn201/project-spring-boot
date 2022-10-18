66de325b5ea4240d7f04e272ad0da22f264cf715
Nam <admin@Nam>
2022-10-18 22:11:09 +0700

    first commit

---
+++ src/main/java/com/example/restful/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.restful.controller;
+
+import com.example.restful.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private List<User> users = new ArrayList<User>();
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+         return users;
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user)
+    {
+        users.add(user);
+        return user;
+    }
+
+    @PutMapping("/user")
+    public void edit(@RequestBody User user)
+    {
+        for (int i = 0; i < users.size(); i++)
+        {
+            if(users.get(i).getId() == user.getId())
+            {
+                users.set(i, user);
+                break;
+            }
+        }
+    }
+
+    @DeleteMapping("/user")
+    public void delete(@RequestParam(name = "id") int id)
+    {
+        for (int i = 0; i < users.size(); i++)
+        {
+            if(users.get(i).getId() == id)
+            {
+                users.remove(i);
+                break;
+            }
+        }
+    }
+}
