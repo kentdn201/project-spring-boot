2678a4240873b39034d7c1ebe7b52f7ec05a8fed
Nam <admin@Nam>
2022-10-18 22:47:14 +0700

    update Controller findUserById

--- src/main/java/com/example/restful/controller/UserController.java
+++ src/main/java/com/example/restful/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 public class UserController {
     private List<User> users = new ArrayList<User>();
@@ -20,6 +21,16 @@ public class UserController {
     {
         users.add(user);
         return user;
+    }
+
+    @GetMapping("/user/{id}")
+    public User findUserById(@PathVariable int id) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == (id)) {
+                return users.get(i);
+            }
+        }
+        return null;
     }
 
     @PutMapping("/user")
